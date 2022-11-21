34c6dc99ba9e3b449709975d0a5dd0a82b68bb29
maxrok98 <max.rok.98@gmail.com>
2022-11-22 00:02:55 +0200

    lab4 - templates and views

--- app_blog/models.py
+++ app_blog/models.py
@@ -6,14 +6,22 @@ from django.utils import timezone
 class Category(models.Model):
     category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
     slug = models.SlugField(u'Слаг')
-    objects = models.Manager
+    objects = models.Manager()
 
     class Meta:
-        verbose_name = u'Категрія для публікацій'
+        verbose_name = u'Категорія для публікацій'
         verbose_name_plural = u'Категорії для публікацій'
 
     def __str__(self):
         return self.category
+
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/"
+
+        return url
 
 
 class Article(models.Model):
